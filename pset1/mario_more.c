80f86c9029b7d543d0b77472d96535a39fba542a
Julien Mihai <mihaijulien@gmail.com>
2022-10-05 16:02:46 +0300

    pset1

---
+++ pset1/mario_more.c
@@ -0,0 +1,35 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Positive number: ");
+    }
+    while (height <= 0 || height > 8);
+
+    int block = 1;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int space = 0; space < height - block; space++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < block; j++)
+        {
+            printf("#");
+        }
+        printf(" ");
+
+        for (int j = 0; j < block; j++)
+        {
+            printf("#");
+        }
+
+        block++;
+        printf("\n");
+    }
+}
